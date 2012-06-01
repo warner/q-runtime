aac7e9be44a70f1382762a285b8f7895973ebdff
Brian Warner <warner@lothar.com>
2012-06-01 00:04:28 -0700

    hush pyflakes

--- spellserver/scripts/poke.py
+++ spellserver/scripts/poke.py
@@ -2,7 +2,6 @@
 import os
 import urllib
 from . import webwait
-from .. import database, nonce
 
 def poke(so, out, err):
     message = so["message"]
@@ -11,8 +10,6 @@ def poke(so, out, err):
     if not (os.path.isdir(basedir) and os.path.exists(dbfile)):
         print >>err, "'%s' doesn't look like a spellserver basedir, quitting" % basedir
         return 1
-    sqlite, db = database.get_db(dbfile, err)
-    c = db.cursor()
     baseurl, vatid = webwait.wait(basedir, err)
     print "Node appears to be running, poking"
     r = urllib.urlopen(baseurl+"poke", message)
